56a3c891693fc476d75fdcfa96d7f871bc1dbb0f
Minahil Azaz <99528192+minahil-azaz@users.noreply.github.com>
2026-05-06 00:24:10 +0500

    Update config.py

--- client/config.py
+++ client/config.py
@@ -3,4 +3,5 @@
 # API_URL = "http://127.0.0.1:8000"
 
 # Uncomment and use this line if you want the frontend to call a deployed backend directly.
-API_URL = "https://medical-rag-ai-assistant-t891.onrender.com"
+# API_URL = "https://medical-rag-ai-assistant-t891.onrender.com"
+API_URL = "https://medical-rag-ai-assistant-1.onrender.com"
